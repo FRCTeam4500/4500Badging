b332f7175a2ebeff967e4e71192131b2d271f340
Saaleh <97705117+SPoovathumkadavil@users.noreply.github.com>
2023-10-20 15:54:52 -0500

    Loading Screens

--- app/(testing)/test/page.tsx
+++ app/(testing)/test/page.tsx
@@ -1,29 +1,13 @@
 "use client"
 
 import { useIsVisible } from "@/hooks/use-is-visible";
+import { Loader2 } from "lucide-react";
 import { useRef } from "react";
 
-export default function Page() {
-    const ref1 = useRef(null);
-    const isVisible1 = useIsVisible(ref1);
-
-    const ref2 = useRef(null);
-    const isVisible2 = useIsVisible(ref2);
-
-    const ref3 = useRef(null);
-    const isVisible3 = useIsVisible(ref3);
-
-    return (
-        <>
-            <div ref={ref1} className={`transition-opacity ease-in duration-700 ${isVisible1 ? "opacity-100" : "opacity-0"}`}>
-                <>...your custom component here...</>
-            </div>
-            <div ref={ref2} className={`transition-opacity ease-in duration-700 ${isVisible2 ? "opacity-100" : "opacity-0"}`}>
-                <>...your custom component here...</>
-            </div>
-            <div ref={ref3} className={`transition-opacity ease-in duration-700 ${isVisible3 ? "opacity-100" : "opacity-0"}`}>
-                <>...your custom component here...</>
-            </div>
-        </>
-    )
+export default function Loading() {
+	return (
+		<div className="h-screen flex items-center justify-center">
+			<Loader2 className="h-4 w-4 animate-spin" />
+		</div>
+	)
 }
